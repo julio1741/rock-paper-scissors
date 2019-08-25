ccefa4fe0c3fe13886c7922fc5a679528f6942bf
julio1741 <julio1741@gmail.com>
2019-08-25 00:35:56 -0300

    Choose winner helper

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,20 @@
 module PlayersHelper
+  def get_moves
+    JSON.parse(File.read('config/moves.json'))["moves"]
+  end
+
+  def choose_winner player1, player2, move1, move2
+    wins = ""
+    moves = get_moves
+    kill1 = moves.select{|m| m["move"] == move1}.first["kills"]
+    kill2 = moves.select{|m| m["move"] == move2}.first["kills"]
+    if kill1 == move2
+      wins = player1
+    elsif kill2 == move1
+      wins = player2
+    else
+      wins = "no_winner"
+    end
+    wins
+  end
 end
